cfa248d9bc89840c6571e251a54b373b576350c0
CodingMarin <marinalexander691@gmail.com>
2023-09-06 13:25:02 -0500

    invert url

--- app/components/shopcard.jsx
+++ app/components/shopcard.jsx
@@ -14,8 +14,8 @@ export default function shopCard({ picture, title, url }) {
             <div class="px-5 pb-5">
                 <h5 class="text-center text-sm font-medium tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 <div class="flex gap-x-5 gap-y-5 items-center justify-center pt-10">
-                    <Link href={url} class="w-full text-white bg-[#BADA55] hover:bg-lime-600 transition duration-150 focus:outline-none focus:ring-0 font-medium rounded-lg text-sm py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Comprar</Link>
-                    <Link href="https://api.whatsapp.com/send/?phone=961361759&text=Estoy interesado en comprar tus productos" class="w-full text-black hover:text-slate-700 ring-[#BADA55] ring-2 hover:bg-lime-100 transition duration-200 focus:ring-0 focus:outline-none font-medium rounded-lg text-sm py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Detalles</Link>
+                    <Link href="https://api.whatsapp.com/send/?phone=961361759&text=Estoy interesado en comprar tus productos" class="w-full text-white bg-[#BADA55] hover:bg-lime-600 transition duration-150 focus:outline-none focus:ring-0 font-medium rounded-lg text-sm py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Comprar</Link>
+                    <Link href={url} class="w-full text-black hover:text-slate-700 ring-[#BADA55] ring-2 hover:bg-lime-100 transition duration-200 focus:ring-0 focus:outline-none font-medium rounded-lg text-sm py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Detalles</Link>
                 </div>
             </div>
         </div>
