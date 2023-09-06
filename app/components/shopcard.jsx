d0a45c87633ff3d246f892de6ff1e6f89d6b0b15
CodingMarin <marinalexander691@gmail.com>
2023-09-06 13:25:33 -0500

    apply border

--- app/components/shopcard.jsx
+++ app/components/shopcard.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function shopCard({ picture, title, url }) {
     return (
-        <div class=" bg-white border border-gray-200 rounded-lg shadow">
+        <div class=" bg-white border rounded-lg shadow">
             <Image
                 class="p-8 rounded-t-lg"
                 src={picture}
