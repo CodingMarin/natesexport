7a6d716422fc5ec45615593daa3b7223c481e885
CodingMarin <marinalexander691@gmail.com>
2023-09-06 13:14:14 -0500

    announcement removed

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className} style={{ background: '#fff' }}>
         <div className="absolute z-[-1] h-screen object-contain w-full opacity-75" style={{ backgroundImage: 'linear-gradient(to right bottom, #000000, transparent, transparent, transparent, transparent)' }}></div>
-        <Announcement></Announcement>
+        {/* <Announcement></Announcement> */}
         {children}
       </body>
     </html>
