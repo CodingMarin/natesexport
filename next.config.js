6c8a67b225985806da250ba698bd21ef43bce796
CodingMarin <marinalexander691@gmail.com>
2023-09-08 18:00:18 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        formats: ['image/webp'],
+    },
+}
