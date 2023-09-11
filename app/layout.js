ad0c83551fbc692bdcfe51282d104a27de6ac3be
CodingMarin <marinalexander691@gmail.com>
2023-09-11 18:25:15 -0500

    favicon add

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <link rel='shortcut icon' href="/next.svg" />
       <body className={inter.className} style={{ background: '#fff' }}>
         <div className="absolute z-[-1] h-screen object-contain w-full opacity-75" style={{ backgroundImage: 'linear-gradient(to right bottom, #000000, transparent, transparent, transparent, transparent)' }}></div>
         {children}
