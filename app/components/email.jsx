f9bf962b4c71e32c2aab008c21c3cd1a83302114
CodingMarin <marinalexander691@gmail.com>
2023-09-15 19:10:09 -0500

    add htmlFor

--- app/components/email.jsx
+++ app/components/email.jsx
@@ -54,21 +54,21 @@ export default function SendEmail() {
             <form onSubmit={handleSubmit}>
 
                 <div className="py-4">
-                    <label for="telefono" className="text-sm text-[#091E42] font-medium">Telefono / Correo</label>
+                    <label htmlFor="telefono" className="text-sm text-[#091E42] font-medium">Telefono / Correo</label>
                     <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 justify-center">
                         <input required type="tel" id="telefono" aria-describedby="helper-text-explanation" value={phone} onChange={(e) => setPhone(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 focus:outline-none" placeholder="Telefono" autoComplete="off" />
                         <input required type="email" id="email" aria-describedby="helper-text-explanation" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 focus:outline-none" placeholder="example@gmail.com" autoComplete="off" />
                     </div>
                 </div>
 
-                <label for="name" className="pb-4 text-sm text-[#091E42] font-medium">Nombres / Apellidos</label>
+                <label htmlFor="name" className="pb-4 text-sm text-[#091E42] font-medium">Nombres / Apellidos</label>
                 <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 justify-center">
                     <input required type="text" id="name" aria-describedby="helper-text-explanation" value={name} onChange={(e) => setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 focus:outline-none" placeholder="Nombres" autoComplete="off" />
                     <input required type="text" id="surname" aria-describedby="helper-text-explanation" value={surname} onChange={(e) => setSurname(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 focus:outline-none" placeholder="Apellidos" autoComplete="off" />
                 </div>
 
                 <div className="py-5">
-                    <label for="message" className="block mb-2 text-sm font-medium text-[#091E42] ">Escribe tu solicitud aqui... *</label>
+                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-[#091E42] ">Escribe tu solicitud aqui... *</label>
                     <textarea required id="message" rows="4" value={message} onChange={(e) => setMessage(e.target.value)} className="block p-2.5 w-full text-sm text-[#091E42] bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Escribe tu solicitud aquí..."></textarea>
                 </div>
 
