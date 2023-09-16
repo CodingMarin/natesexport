8b932a3583568b9b9a4cf83079d3536ae10d58fe
CodingMarin <marinalexander691@gmail.com>
2023-09-15 22:16:35 -0500

    fixed country

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ export default createMiddleware({
     locales: ['en', 'es'],
 
     // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-    defaultLocale: 'en'
+    defaultLocale: 'es'
 });
 
 export const config = {
