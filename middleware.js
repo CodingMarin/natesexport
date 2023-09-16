1c6403f68006e1e5f2778fc501fa8f450c6d9a3a
CodingMarin <marinalexander691@gmail.com>
2023-09-15 22:22:10 -0500

    change es

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
     // A list of all locales that are supported
-    locales: ['en', 'es'],
+    locales: ['es', 'en'],
 
     // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
     defaultLocale: 'es'
