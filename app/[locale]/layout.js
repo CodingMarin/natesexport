7796816844c3312c37cea48ca3e0fb79b4efa909
CodingMarin <marinalexander691@gmail.com>
2023-09-15 20:26:31 -0500

    fixed locale

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -2,6 +2,7 @@ import "../globals.css"
 import { useLocale } from "next-intl"
 import { notFound } from "next/navigation"
 import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: {
@@ -47,17 +48,16 @@ export const metadata = {
   author: "Nates Export",
 }
 
-const inter = Inter({ subsets: ["latin"] })
 
-export default function RootLayout({ children, params }) {
-  const local = useLocale()
+export default function LocaleLayout({ children, params }) {
+  const locale = useLocale()
   console.log('languages:', params.locale)
 
   if (params.locale !== locale) {
     notFound();
   }
   return (
-    <html lang={local} className={inter.className}>
+    <html lang={locale} className={inter.className}>
       <body className={inter.className} style={{ background: "#fff" }}>
         <div className="absolute z-[-1] h-screen object-contain w-full opacity-75 bg-gradient-nolinear" />
         {children}
