015817e19973bd16dab6034a5c3077affcc507b3
CodingMarin <marinalexander691@gmail.com>
2023-09-22 04:26:47 -0500

    add in feed card

---
+++ app/[locale]/components/adbinfeed.jsx
@@ -0,0 +1,11 @@
+export default function AdbFeed() {
+    return (
+        <div>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1618551591765757" crossOrigin="anonymous"></script>
+            <ins class="adsbygoogle" data-ad-format="fluid" data-ad-layout-key="-cp-2j+1u-1k+c7" data-ad-client="ca-pub-1618551591765757" data-ad-slot="8156349357" style={{ display: 'Block' }}></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({ });
+            </script>
+        </div>
+    )
+}
