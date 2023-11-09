61a4f476538c6f10f26890f165257daac6e5a868
CodingMarin <marinalexander691@gmail.com>
2023-11-09 11:39:06 -0500

    Fixed slide

--- app/[locale]/utils/products.jsx
+++ app/[locale]/utils/products.jsx
@@ -4,35 +4,30 @@ const productsp = [
         name: "Orégano Orgánico",
         url: "/shop/product/oregano",
         img: "/oregano_min.webp",
-        price: 100,
     },
     {
         id: 2,
         name: "Cúrcuma",
         url: "/shop/product/curcuma",
         img: "/curcuma_peruana.webp",
-        price: 100,
     },
     {
         id: 3,
         name: "Aceitunas",
         url: "/shop/product/aceitunas",
         img: "/aceitunas_peruanas.webp",
-        price: 100,
     },
     {
         id: 4,
         name: "Hongo Deshidratado",
         url: "/shop/product/hongo",
-        img: "/hongo_deshidratado_min.jpg",
-        price: 100,
+        img: "/hongo_deshidratado_min.webp",
     },
     {
         id: 5,
         name: "Café Orgánico",
         url: "/shop/product/cafe",
-        img: "/cafe_peruano.jpg",
-        price: 100,
+        img: "/cafe_peruano.webp",
     }
 ]
 
